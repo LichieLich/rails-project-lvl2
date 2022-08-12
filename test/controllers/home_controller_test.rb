883b58d2a17426081e1fff304be4014e30d55bb0
LichieLich <lichie_lich@mail.ru>
2022-08-13 00:42:16 +0300

    Fixed tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,7 +4,14 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get :index' do
-    get home_: index_url
+    sign_in users(:user_one)
+    get root_path
     assert_response :success
+  end
+
+  test 'should redirect unauthrorized user' do
+    sign_out users(:user_one)
+    get root_path
+    assert_response :redirect
   end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,11 +5,8 @@ require 'test_helper'
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @post = posts(:one)
-  end
-
-  test 'should get index' do
-    get posts_url
-    assert_response :success
+    @user = users(:user_one)
+    sign_in @user
   end
 
   test 'should get new' do
@@ -18,8 +15,18 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should create post' do
+    # Работает
+    post = Post.new body: @post.body, creator: @user.email, title: @post.title, category: @post.category
+    post.save # => true
+
+    # Не рабоатет. Количество постов не увеличивается, так как не передаётся категория в параметры запроса
     assert_difference('Post.count') do
-      post posts_url, params: { post: { body: @post.body, creator: @post.creator, title: @post.title } }
+      p @post.valid?
+      p @post.errors
+      # Как должно бы работать
+      # post posts_url, params: { post: { body: @post.body, creator: @user.email, title: @post.title, category: @post.category } }
+      # Как работает в итоге
+      post posts_url, params: { post: { body: @post.body, creator: @user.email, title: @post.title, category_id: @post.category.id } }
     end
 
     assert_redirected_to post_url(Post.last)
@@ -36,8 +43,15 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update post' do
-    patch post_url(@post), params: { post: { body: @post.body, creator: @post.creator, title: @post.title } }
+    new_post = posts(:two)
+    # patch post_url(@post), params: { post: { body: @post.body, creator: "sdadasd", title: @post.title, category: @post.category } }
+    patch post_url(@post), params: { post: { body: new_post.body, creator: new_post.creator, title: new_post.title, category_id: new_post.category.id } }
     assert_redirected_to post_url(@post)
+
+    @post = Post.find(@post.id)
+    assert { @post.body == new_post.body }
+    assert { @post.creator == new_post.creator }
+    assert { @post.title == new_post.title }
   end
 
   test 'should destroy post' do
@@ -45,6 +59,6 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
       delete post_url(@post)
     end
 
-    assert_redirected_to posts_url
+    assert_redirected_to root_path
   end
 end
